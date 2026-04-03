9204877a5c123b9396dfd9a9a64682b7d6c73e7f
Ulysses Zhan <UlyssesZhan@gmail.com>
2026-04-02 22:47:17 -0700

    genericUnpackHook: strip multiple layers of dirs

--- pkgs/genericUnpackHook/setup-hook.sh
+++ pkgs/genericUnpackHook/setup-hook.sh
@@ -2,16 +2,13 @@ unpackCmdHooks+=(_try7zz)
 
 _try7zz() {
   7zz x $curSrc -osource
-  files=(source/*)
-  if [[ ${#files[@]} -eq 1 && -d "${files[0]}" ]]; then
-    dir="$(basename "${files[0]}")"
-    if [[ "$dir" == source ]]; then
-      mv source source-renamed
-      mv source-renamed/source source
-      rm -r source-renamed
-    else
-      mv "source/$dir" .
-      rm -r source
+  while true; do
+    files=(source/*)
+    if [[ ! ${#files[@]} -eq 1 || ! -d "${files[0]}" ]]; then
+      break
     fi
-  fi
+    mv "${files[0]}" new-source
+    rm -r source
+    mv new-source source
+  done
 }
